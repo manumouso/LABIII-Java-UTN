500a604e15747f47c17a1e97b166294bf3fb35ac
manumouso <63980825+manumouso@users.noreply.github.com>
2021-05-07 22:22:52 -0300

    primary stage, join room

---
+++ KokumoNoMonogatari/src/edu/utn/Main.java
@@ -0,0 +1,15 @@
+package edu.utn;
+
+import edu.utn.view.PrimaryStage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PrimaryStage primaryStage = new PrimaryStage();
+        primaryStage.menu();
+        primaryStage.footer();
+    }
+
+
+}
---
+++ KokumoNoMonogatari/src/edu/utn/view/Menu.java
@@ -0,0 +1,8 @@
+package edu.utn.view;
+
+public interface Menu {
+    void header();
+    void menu();
+    void footer();
+
+}
