fba8c9ab18e7fa5f055fe4eca00f0e66cb9b584c
manumouso <63980825+manumouso@users.noreply.github.com>
2021-05-07 23:23:01 -0300

    intro

--- KokumoNoMonogatari/src/edu/utn/Main.java
+++ KokumoNoMonogatari/src/edu/utn/Main.java
@@ -1,11 +1,12 @@
 package edu.utn;
 
+import edu.utn.view.Introduction;
 import edu.utn.view.PrimaryStage;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Introduction.print();
         PrimaryStage primaryStage = new PrimaryStage();
         primaryStage.menu();
         primaryStage.footer();
---
+++ KokumoNoMonogatari/src/edu/utn/view/Introduction.java
@@ -0,0 +1,29 @@
+package edu.utn.view;
+
+import java.util.Scanner;
+
+public class Introduction {
+
+    public final static void print(){
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("\n     ======================================================================\n");
+        System.out.println("\t\t\t Puede que mi rostro haya cambiado,");
+        System.out.println("\t\t\t han ocurrido muchas cosas, y hay muchos secretos,");
+        System.out.println("\t\t\t muchos han sido olvidados,");
+        System.out.println("\t\t\t simplemente los han abandonado.");
+        System.out.println("\t\t\t Si sigues los pasos de aquella persona,");
+        System.out.println("\t\t\t te espera un camino dificil,");
+        System.out.println("\t\t\t si asi lo quiere el destino, nos volveremos a ver");
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("\n");
+        System.out.print("\t\t\t Ingrese un caracter para continuar-> ");
+        Scanner scanner =new Scanner(System.in);
+        scanner.next();
+
+
+
+    }
+}
