550667644620a58b62aeff6d2cef88dc86ce845d
manumouso <63980825+manumouso@users.noreply.github.com>
2021-05-09 12:18:16 -0300

    reset config

--- KokumoNoMonogatari/src/edu/utn/view/Main.java
+++ KokumoNoMonogatari/src/edu/utn/view/Main.java
@@ -1,12 +1,11 @@
-package sample;
+package edu.utn.view;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
     Button button;
