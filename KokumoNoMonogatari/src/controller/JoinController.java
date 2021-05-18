16df0fbcde515ade12619506f76f2e5e3fedda6e
manumouso <63980825+manumouso@users.noreply.github.com>
2021-05-18 09:20:44 -0300

    thread added to joinController

--- KokumoNoMonogatari/src/controller/JoinController.java
+++ KokumoNoMonogatari/src/controller/JoinController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -12,8 +15,10 @@ import java.util.ResourceBundle;
 
 public class JoinController implements Initializable {
 
-    public Button goBackButton;
+    public Button goBackButton,joinButton;
     private Scene accessScene;
+    public TextArea messagesTxtArea;
+    private Service<Void> backgroundThread;
 
     public void setAccessScene(Scene scene) {
         accessScene = scene;
@@ -27,5 +32,34 @@ public class JoinController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void clickedJoinServer(ActionEvent event){
+        backgroundThread= new Service<Void>() {
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        //Here call clientController
+                        updateMessage("Connection established, Client Ready ");
+                        return null;
+                    }
+                };
+            }
+        };
+        backgroundThread.setOnSucceeded(workerStateEvent -> {
+            System.out.println("Connection ended successfully");
+            //unbind to prevent Exceptions, next time the textArea has some message appended main thread
+            messagesTxtArea.textProperty().unbind();
+        });
+        backgroundThread.setOnFailed(workerStateEvent -> {
+            System.out.println("Connection lost");
+            messagesTxtArea.textProperty().unbind();
+        });
+        //the text area from the GUI thread/ main thread is bound to the updateMessage
+        messagesTxtArea.textProperty().bind(backgroundThread.messageProperty());
+        //this function starts the thread
+        backgroundThread.restart();
     }
 }
