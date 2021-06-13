f98718e440cdc444dee509e715a3668d387a2eff
manumouso <63980825+manumouso@users.noreply.github.com>
2021-06-12 21:58:52 -0300

    client
    next: 1) do some request
    2)client mode view
    3)test a game cycle

--- Kokumo/src/edu/utn/connection/client/Client.java
+++ Kokumo/src/edu/utn/connection/client/Client.java
@@ -1,4 +1,91 @@
 package edu.utn.connection.client;
 
+
+import edu.utn.json.Constants;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+
 public class Client {
+
+    private int connectionTimeout = 20000; // in milliseconds
+    private int dataRetrievalTimeout = 20000; // in milliseconds
+    private boolean followRedirects = false; // automatically follow HTTP redirects
+
+    public Client() {
+
+    }
+
+    protected void request(String url, ResponseHandler handler,String jsonInput) {
+
+        HttpURLConnection urlConnection = null;
+
+
+        try {
+            URL resourceUrl = new URL(url);
+            urlConnection = (HttpURLConnection) resourceUrl.openConnection();
+
+            // Settings
+            urlConnection.setConnectTimeout(connectionTimeout);
+            urlConnection.setReadTimeout(dataRetrievalTimeout);
+            urlConnection.setUseCaches(false);
+            urlConnection.setInstanceFollowRedirects(followRedirects);
+            urlConnection.setRequestMethod(Constants.POST);
+            urlConnection.setDoInput(true);
+
+            // Header
+            urlConnection.setRequestProperty(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON);
+
+
+            handler.onStart(urlConnection);
+
+            // Request Body
+            // POST JSON
+            urlConnection.setDoOutput(true);
+
+            String jsonInputString = "{\"name\": \"Upendra\", \"job\": \"Programmer\"}";
+            try(OutputStream os = urlConnection.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                urlConnection.setFixedLengthStreamingMode(input.length);
+                os.write(input);
+            }
+
+            // Process the response in the handler because it can be done in different ways
+            handler.processResponse(urlConnection);
+            // Request finished
+            handler.onFinish(urlConnection);
+
+        } catch (IOException e) {
+            handler.onFailure(e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+    }
+
+   public void post(String url, ResponseHandler handler,String input) {
+        request(url, handler,input);
+    }
+
+    public int getConnectionTimeout() {
+        return connectionTimeout;
+    }
+
+    public void setConnectionTimeout(int connectionTimeout) {
+        this.connectionTimeout = connectionTimeout;
+    }
+
+    public int getDataRetrievalTimeout() {
+        return dataRetrievalTimeout;
+    }
+
+    public void setDataRetrievalTimeout(int dataRetrievalTimeout) {
+        this.dataRetrievalTimeout = dataRetrievalTimeout;
+    }
+
 }
---
+++ Kokumo/src/edu/utn/connection/client/ResponseHandler.java
@@ -0,0 +1,84 @@
+package edu.utn.connection.client;
+
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+
+public abstract class ResponseHandler {
+
+    protected static int BUFFER_SIZE = 1024 * 8; // Size of the buffer when reading data from output stream
+
+
+    public void onStart(HttpURLConnection httpURLConnection) {
+        // Do  nothing by default
+    }
+
+
+    public void onFinish(HttpURLConnection httpURLConnection) {
+        // Do nothing by default
+    }
+
+    public abstract void onSuccess(int statusCode, Map<String, List<String>> headers, byte[] content);
+
+    public abstract void onFailure(int statusCode, Map<String, List<String>> headers, byte[] content);
+
+    public abstract void onFailure(Throwable throwable);
+
+    public void onProgressChanged(long bytesReceived, long totalBytes) {
+        // Default, do nothing.
+    }
+
+    protected byte[] readFrom(InputStream inputStream, long length) throws IOException {
+        if (inputStream == null) return new byte[0];
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer, 0, buffer.length)) != -1) {
+            os.write(buffer, 0, bytesRead);
+            onProgressChanged(bytesRead, length);
+        }
+        os.flush();
+        os.close();
+        return os.toByteArray();
+    }
+
+    //puedo devolver el String y despues parsearlo, pero creo que imprimiendo el mensaje ya estoy
+    protected void readToString(HttpURLConnection connection){
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void processResponse(HttpURLConnection connection) {
+        try {
+            // Response
+            int responseCode = connection.getResponseCode();
+            long contentLength = connection.getContentLength();
+            Map<String, List<String>> responseHeaders = connection.getHeaderFields();
+
+            // Successful response codes will be in interval [200,300)
+            if (responseCode >= 200 && responseCode < 300) {
+                byte[] responseContent = readFrom(connection.getInputStream(), contentLength);//or print the json as if with readToString()
+                onSuccess(responseCode, responseHeaders, responseContent);
+            } else {
+                byte[] responseContent = readFrom(connection.getErrorStream(), contentLength);
+                onFailure(responseCode, responseHeaders, responseContent);
+            }
+        } catch (IOException e) {
+            onFailure(e);
+        }
+    }
+}
